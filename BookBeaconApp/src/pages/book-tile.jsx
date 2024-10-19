0066ea901cc8b32c7a3281d2f55f61fd2b989788
Akhilaknair <akhilaknair24@gmail.com>
2024-10-19 11:04:55 +0530

    edited

--- BookBeaconApp/src/pages/book-tile.jsx
+++ BookBeaconApp/src/pages/book-tile.jsx
@@ -1,3 +1,6 @@
+import { Badge } from "@/components/ui/badge"
+import books from  '../index.jsx'
+
 function ShoppingBookTile({books})
 {
     return(
@@ -6,7 +9,17 @@ function ShoppingBookTile({books})
                     <div className="relative">
                         <img src={books?.image}
                         className="w-dull h-[300px] object-cover rounded-t-lg"/>
+                       {
+                          books?.price>0 ?(
+                       <Badge className='absolute top-2 left-2 bg-red-500 hover:bg-red-600'>buy</Badge>):null
+                       }
+
                     </div>
+
+                    <CardContent className='p-4'>
+                        <h2 className="text-xl font-bold mb-2">{books?.name}</h2>
+                        <div> </div>
+                        </CardContent>
                 </div>         
                 </Card>
     );
